cd88bedc83ec595174778a511c69d2371d772548
Aeases <96164686+Aeases@users.noreply.github.com>
2025-11-24 21:59:54 +0800

    moved database init into seperate files

---
+++ database_initialisations.py
@@ -0,0 +1,56 @@
+table_definitions = [
+    """CREATE TABLE EMPLOYEE(
+        employee_id INT PRIMARY KEY IDENTITY(1,1),
+        e_name, 
+        e_phone, 
+        FOREIGN KEY (supervisor_id) REFERENCES EMPLOYEE(employee_id), 
+        level
+    )""",
+
+    """CREATE TABLE CMM_ACTIVITY(
+        activity_id,
+        activity_desc,
+        start_date,
+        end_date,
+        makes_unusable,
+        category
+    )""",
+
+    """CREATE TABLE SUBCONTRACTOR(
+        c_id,
+        c_name,
+        c_phone,
+        c_email
+    )""",
+
+    """CREATE TABLE E_ASSIGNMENT(
+        a_id, 
+        e_id
+    )""",
+
+    """CREATE TABLE C_ASSIGNMENT(
+        a_id, 
+        c_id
+    )""",
+    """CREATE TABLE CAMPUS_AREA(
+        a_id,
+        c_id
+    )""",
+    """CREATE TABLE CHEMICALS(
+        chemical_id,
+        chemical_name,
+        is_harmful
+    )""",
+    """CREATE TABLE CHEMICAL_USAGE(
+        chemical_id,
+        a_id
+    )""",
+    """CREATE TABLE BUILDING_SUPERVISION(
+        building_id,
+        e_id
+    )"""
+]
+
+employee_definitions = [
+    ""
+]
--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import sqlite3 as sql
-
+import os
+from database_initialisations import table_definitions, employee_definitions
 con = sql.connect("database.db")
 cur = con.cursor()
 
-table_definitions = [
-    "CREATE TABLE EMPLOYEE(employee_id, e_name, e_phone, supervisor_id, level)",
-    "CREATE TABLE CMM_ACTIVITY(activity_id, activity_desc, start_date, end_date, makes_unusable, category)",
-    "CREATE TABLE SUBCONTRACTOR(c_id, c_name, c_phone, c_email)",
-    "CREATE TABLE E_ASSIGNMENT(a_id, e_id)",
-    "CREATE TABLE C_ASSIGNMENT(a_id, c_id)",
-    "CREATE TABLE CAMPUS_AREA(a_id, c_id)",
-    "CREATE TABLE CHEMICALS(chemical_id, chemical_name, is_harmful)",
-    "CREATE TABLE CHEMICAL_USAGE(chemical_id, a_id)",
-    "CREATE TABLE BUILDING_SUPERVISION(building_id, e_id)"
-]
 
-for table in table_definitions:
-    cur.execute(table) 
+
+if not (os.path.exists("./database.db")):
+    for table in table_definitions:
+        cur.execute(table) 
 
 def getTableNames():
     cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
@@ -29,13 +21,13 @@ def getTableColumns(table_name):
     return table_columns
 
 def getUserInput():
-    print("\033[33;4m", "")
-    input("")
-    print("\033[0m", "")
-
+    print("\033[33;4m", end="")
+    res = input("")
+    print("\033[0m", end="")
+    return res
 
 print(getTableColumns(getTableNames()[0]))
 
-print("Employee ID ↓ ", "");
-getUserInput()
+print("Employee ID: ", end="");
+employeeID = getUserInput()
 
