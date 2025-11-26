034406632d65c7e7fa625c7452231ae176b8b7ce
perli27 <pearlyy.richell@gmail.com>
2025-11-26 20:04:49 +0800

    fix a little

--- add_tuples.py
+++ add_tuples.py
@@ -29,5 +29,5 @@ def add_tuple_to_sql(target_table, tuple):
 
     escaped_values = [escapeString(t) for t in tuple]
     
-    insertion_string = f"INSERT INTO {target_table} ({", ".join(getTableColumns(target_table))}) VALUES ({", ".join(escaped_values)})"
+    insertion_string = f"INSERT INTO {target_table} ({', '.join(getTableColumns(target_table))}) VALUES ({', '.join(escaped_values)})"
     cur.execute(insertion_string)
