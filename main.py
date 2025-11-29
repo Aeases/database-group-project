aeb8458508f581a209da317fc95b6bf4fcea04b6
valenciasnts <valenciasnts06@gmail.com>
2025-11-30 04:32:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from db import cur, con
 from add_tuples import user_add_tuple
+from add_tuples import set_based_insertion
 from updating_tuples import user_update_tuple
 from updating_tuples import user_delete_tuple
 from utils import getUserInput, getTableUserInput
 from sqlite_print import print_cursor
 from scheduled_activities import getScheduledActivities
 from report_generation import generate_reports
+import sys
 
 def show_menu():
     print("""
     1 | Insert Tuples
-    2 | Update Tuples
-    3 | Delete Tuples
-    4 | Run SQL Queries
-    5 | Find Scheduled Cleaning Activities
-    6 | Generate Reports
+    2 | Set-based Insertion
+    3 | Update Tuples
+    4 | Delete Tuples
+    5 | Run SQL Queries
+    6 | Find Scheduled Cleaning Activities
+    7 | Generate Reports
+    8 | Exit System      
     """)
 
 
@@ -25,33 +29,43 @@ def process_cmd():
 
     while True:
         choice = getUserInput("Option: ")
-        if len(choice) == 1 and choice in ['1', '2', '3', '4', '5', '6']:
+        if len(choice) == 1 and choice in ['1', '2', '3', '4', '5', '6', '7', '8']:
             break;
         else:
             print("Invalid Input")
 
 
     table_choice = ''
-    if choice in ['1', '2', '3']:
+    if choice in ['1', '2', '3', '4']:
         table_choice = getTableUserInput("Table: ")
 
     match choice:
         case '1':
             user_add_tuple(table_choice)
         case '2':
-            user_update_tuple(table_choice)
+            set_based_insertion(table_choice)    
         case '3':
-            user_delete_tuple(table_choice)
+            user_update_tuple(table_choice)
         case '4':
-            user_SQL_query = getUserInput("SQL QUERY: ", ansiStart='')
-            print("-- Result --\n")
-            print_cursor(cur.execute(user_SQL_query))
+            user_delete_tuple(table_choice)
         case '5':
-            getScheduledActivities()
+            try:
+                user_SQL_query = getUserInput("SQL QUERY: ", ansiStart='')
+                print("-- Result --\n")
+                print_cursor(cur.execute(user_SQL_query))
+            except Exception as e:
+                print(f"SQL Error: {e}")
+                con.rollback()
         case '6':
+            getScheduledActivities()
+        case '7':
             generate_reports()
+        case '8':
+            print("Thank you for using Campus Maintenance Management System. Goodbye!")
+            sys.exit(0)
     con.commit()
 
 if __name__ == "__main__":
+    print("Welcome to Campus Maintenance Management System!")
     while True:
         process_cmd()
